db0c66c2c467478f54e8e3f170a7534682d70b42
KaiWelu <kaiweluda@gmail.com>
2024-09-02 13:06:30 +0200

    adds font and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,19 @@ const config: Config = {
         './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                // this adds the color scheme of the corporate design
+                'primary-1': '#414042',
+                'primary-2': '#9f0524',
+                'secondary-1': '#939598',
+                // this is an example
+                'light-4': '#5C5C7B',
+            },
+            fontFamily: {
+                commissioner: ['Commissioner', 'sans-serif'],
+            },
+        },
     },
     plugins: [nextui()],
 }
