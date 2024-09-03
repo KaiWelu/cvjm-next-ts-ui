f2fc25a18783d044a01ce85dbb888fe3eaeaa256
KaiWelu <kaiweluda@gmail.com>
2024-09-03 10:05:14 +0200

    adds static menu items

--- components/ui/DesktopNav.tsx
+++ components/ui/DesktopNav.tsx
@@ -4,11 +4,48 @@ import Brand from './Brand'
 import { MENU_ITEMS } from '../../lib/menuItems'
 import { MenuItem } from '@/lib/types'
 import MenuItemDropdown from './MenuItemDropdown'
+import {
+    Button,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownTrigger,
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+} from '@nextui-org/react'
 
 const DesktopNav = () => {
     console.log(MENU_ITEMS)
     return (
-        <nav className="relative w-full bg-white p-4 shadow-sm shadow-slate-300">
+        <Navbar className="p-5 shadow-sm shadow-slate-300">
+            <NavbarBrand>
+                <Brand />
+            </NavbarBrand>
+            <NavbarContent className="justify-start">
+                <Dropdown type="listbox" className="border-r-0">
+                    <NavbarItem>
+                        <DropdownTrigger>
+                            <Button
+                                radius="none"
+                                disableRipple={true}
+                                className="bg-primary-2"
+                            >
+                                Button
+                            </Button>
+                        </DropdownTrigger>
+                    </NavbarItem>
+                    <DropdownMenu>
+                        <DropdownItem>Item 1</DropdownItem>
+                        <DropdownItem>Item 2</DropdownItem>
+                        <DropdownItem>Item 3</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+            </NavbarContent>
+        </Navbar>
+    )
+} /* <nav className="relative w-full bg-white p-4 shadow-sm shadow-slate-300">
             <div className="flex w-full flex-row gap-8 px-1">
                 <Brand />
                 <div className="flex flex-col justify-center">
@@ -22,8 +59,6 @@ const DesktopNav = () => {
                     ))}
                 </div>
             </div>
-        </nav>
-    )
-}
+        </nav> */
 
 export default DesktopNav
--- lib/menuItems.ts
+++ lib/menuItems.ts
@@ -1,9 +1,35 @@
 // this file contains static data for the menu items
 
+import path from 'path'
+
 export const MENU_ITEMS = [
     {
         name: 'Haus',
         path: '/haus',
-        items: ['Über uns'],
+        items: [
+            'Über uns',
+            'Austtatung',
+            'Tagungen',
+            'Anfahrt',
+            'Kontakt',
+            'Stellenangebote',
+            'Webcam',
+        ],
     },
+    {
+        name: 'Wassersport',
+        path: '/wassersport',
+        items: ['Kanu', 'Segeln'],
+    },
+    {
+        name: 'Buchen',
+        path: '/buchen',
+        items: ['Kalender', 'Anfahrt', 'Preise'],
+    },
+    {
+        name: 'Umgebung',
+        path: '/umgebung',
+        items: ['Ratzburg', 'Umland', 'Mölln', 'Lübeck'],
+    },
+    { name: 'Förderverein', path: '/verein', items: ['Über uns', 'Spenden'] },
 ]
