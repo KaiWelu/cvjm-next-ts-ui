8cfecce9958672578f758788d2c1978ee22cab8a
KaiWelu <kaiweluda@gmail.com>
2024-09-20 17:34:06 +0200

    adds content for wassersport section

---
+++ app/wassersport/page.tsx
@@ -0,0 +1,133 @@
+const page = () => {
+    return (
+        <main className="flex w-full justify-center p-5">
+            <div className="prose prose-md prose-headings:text-primary-2">
+                <section>
+                    <h1>Wassersportprogramm</h1>
+                    <p className="lead">
+                        Das CVJM Freizeit- und Segelzentrum steht für die Arbeit
+                        mit Kindern, Jugendlichen und jungen Erwachsenen sowie
+                        benachteiligten Menschen und fördert mit seinen
+                        Programmen die sportliche, ökologische und politische
+                        Jugendbildung.
+                    </p>
+                    <p className="lead">
+                        Gerade wenn es um das Zusammenarbeiten verschiedener
+                        Mentalitäten und Personen geht, erweist sich der
+                        Segelsport als sehr integrativ und fördert den
+                        Gruppenprozess - ganz nach dem Motto
+                    </p>
+                </section>
+                <section>
+                    {/*  <h2>
+                        &quot;Gemeinsam in einem Boot das Ziel erreichen&quot;
+                    </h2> */}
+                    <h2>Kursangebot</h2>
+                    <p className="text-xl">
+                        Wir bieten folgende Programme unter Anleitung erfahrener
+                        Segel- und Surflehrer:
+                    </p>
+                    {/*     <ul className="list-outside list-disc marker:size-6 marker:p-5 marker:text-danger-foreground">
+                        <li className="">Segelgrundkurs</li>
+                        <li>Segeln für Fortgeschrittene</li>
+                        <li>Barrierefreies Segeln</li>
+                        <li>Mannschaftskuttersegeln</li>
+                        <li>Surfgrundkurs</li>
+                        <li>Kanuprogramm</li>
+                        <li>Stand Up Paddling</li>
+                    </ul> */}
+                </section>
+                <section>
+                    <h3>Segelgrundkurs und Segelgrundschein</h3>
+                    <p>
+                        In diesem Kurs lernen die Segelschüler die Grundlagen
+                        des Segelns kennen. Er dauert in der Regel vier bis
+                        sechs Tage und eignet sich optimal für Anfänger,
+                        Schulklassen ab der 8. Klasse und Jugendgruppen.
+                    </p>
+                </section>
+                <section>
+                    <h3>Segeln für Fortgeschrittene</h3>
+                    <p>
+                        In diesem Kurs wollen wir die Grundkenntnisse vertiefen
+                        und erweitern. Dabei geht es zum Beispiel um das
+                        verstärkte Einüben von Sicherheitsmanövern wie dem
+                        Boje-über-Bord-Manöver oder das Aufrichten der Boote
+                        nach einer Kenterung. Auch das Regattasegeln kann in den
+                        Fokus treten.
+                    </p>
+                </section>
+                <section>
+                    <h3>Barrierefreies Segeln</h3>
+                    <p>
+                        Eine Besonderheit ist unser Wassersportangebot für
+                        körperlich und geistig benachteiligte Menschen. Auf gut
+                        geeignetem Bootsmaterial erleben die Teilnehmer das
+                        Element Wasser von seiner sicheren Seite und können
+                        anhand praktischer Übungen und Aktionen die eigenen
+                        Grenzen erkunden. Qualifizierte Segellehrer begleiten
+                        diese Maßnahmen in Absprache mit den Gruppenbetreuern.
+                        Wir beraten Sie gerne.
+                    </p>
+                </section>
+                <section>
+                    <h3>Mannschaftskuttersegeln</h3>
+                    <p>
+                        Auf unserem Kutter &quot;Alte Liebe&quot; können 15-20
+                        Personen gemeinsam über den Ratzeburger See segeln.
+                        Gerade für eine gemeinsame Förderung des Team- und
+                        Mannschaftsgeists in einer größeren Gruppe bietet sich
+                        das Kuttersegeln an.
+                    </p>
+                </section>
+                <section>
+                    <h3>Surfgrundkurs und Surfgrundschein</h3>
+                    <p>
+                        Diese Individualsportart ist gerade bei Jugendlichen
+                        sehr beliebt. Nicht nur der richtige Umgang mit Rigg und
+                        Brett wird vermittelt, sondern auch grundlegende
+                        Kenntnisse wie Fahrtechniken und sicheres Verhalten.
+                    </p>
+                </section>
+                <section>
+                    <h3>Stand Up Paddling</h3>
+                    <p>
+                        Es stehen eine Anzahl SUP Boards zur Verfügung, um sich
+                        in dieser coolen Sportart zu versuchen. Auch Yoga wird
+                        gern auf diesen Boards praktiziert.
+                    </p>
+                </section>
+                <section>
+                    <h3>Kanuprogramm</h3>
+                    <p>
+                        Richtig paddeln und steuern eines Kanus will gelernt
+                        sein. Gerade auf den herrlichen Kanuwanderstrecken in
+                        und um Ratzeburg bietet es sich an, diese klassische
+                        Sportart näher kennen zu lernen. Zum Programm gehören
+                        auch Wettfahrten, Kanuspiele und Wanderfahrten. Gerade
+                        junge Schulklassen nutzen diese Möglichkeit gerne.
+                    </p>
+                </section>
+                <section>
+                    <h2>Programmablauf</h2>
+                    <p>
+                        Unsere Wassersportaktivitäten beginnen vormittags um
+                        9:30 Uhr und nachmittags um 14:30 Uhr. Jeder ist
+                        verpflichtet, unsere Sicherheitsregeln und die
+                        Wassersportordnung einzuhalten. Hierzu gehört das
+                        ordnungsgemäße Tragen einer Schwimmweste, die ebenso wie
+                        ein Neoprenanzug zum Surfen, jedem Teilnehmer kostenlos
+                        zur Verfügung gestellt wird. Regenkleidung und festes
+                        Schuhwerk (Sportschuhe) sind selbst mitzubringen.
+                        Meistens haben wir schönes Wetter! Aber bitte beachten
+                        Sie, dass die Wassersportkurse vom Wetter abhängig sind
+                        und dass das Segeln aufgrund von schlechtem Wetter,
+                        Starkwind oder Flaute zeitweise nicht möglich ist.
+                    </p>
+                </section>
+            </div>
+        </main>
+    )
+}
+
+export default page
